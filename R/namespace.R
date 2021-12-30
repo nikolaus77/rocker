c48e184cc4c2c251955a576248a50e7da5632d43
Nikolaus Pawlowski <niko@fr33.net>
2021-12-30 09:32:55 +0100

    Adjusted namespace import -> importFrom

--- R/namespace.R
+++ R/namespace.R
@@ -1,5 +1,5 @@
 
-#' @import DBI
-#' @import R6
-#' @import sodium
+#' @importFrom DBI dbUnloadDriver dbCanConnect dbConnect dbDisconnect dbSendQuery dbSendStatement dbFetch dbHasCompleted dbGetRowsAffected dbGetRowCount dbColumnInfo dbGetStatement dbClearResult dbBegin dbCommit dbRollback dbGetInfo dbIsValid dbCreateTable dbAppendTable dbWriteTable dbReadTable dbRemoveTable dbExistsTable dbListFields dbListObjects dbListTables
+#' @importFrom R6 R6Class
+#' @importFrom sodium keygen data_encrypt data_decrypt
 NULL
