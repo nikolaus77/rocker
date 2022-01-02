3d558f8cec77839d8c288ac6d967bfebe35d88ed
Nikolaus Pawlowski <niko@fr33.net>
2022-01-02 11:23:08 +0100

    Additional documentation

---
+++ R/rocker-package.R
@@ -0,0 +1,24 @@
+
+#' @details
+#' \itemize{
+#'   \item Description of 'rocker' R6 class: \link[rocker:rocker-R6-class]{rocker-R6-class}
+#'   \item Description of S3 functions: \link[rocker:rocker-S3-functions]{rocker-S3-functions}
+#'   \item How-to and examples: \link[rocker:rocker-README]{README}
+#'   \item Additional information and vignettes: \href{https://cran.r-project.org/package=rocker}{CRAN}
+#' }
+#' @keywords internal
+#' @family rocker
+#' @family rocker-S3-functions
+#' @aliases rocker-package
+"_PACKAGE"
+
+#' README
+#'
+#' How-to and examples
+#' @details
+#' Please read me.
+#' @includeRmd README.Rmd
+#' @name rocker-README
+#' @family rocker
+#' @family rocker-S3-functions
+NULL
